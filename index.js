d9a62f9a9b506a51c2634ed25531f8752a565054
Juan Espasa <jrespasa@gmail.com>
2020-12-14 21:54:42 -0300

    modelo sequelize

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const logger = require("morgan");
 const bodyParser = require("body-parser");
 const http = require("http");
 
+const sequelize = require("sequelize");
+const Nuevos = require("./models").nuevos;
+
 // variables de entorno:
 require("dotenv").config();
 
@@ -13,17 +16,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+// rutas y funciones
 app.get("/", function(req, res) {
-    res.json("Hola, llegue")
+    return Nuevos.findAll({})
+    .then(Nuevos => res.status(200).send(Nuevos))
+    .catch(error => res.status().send(error))
 });
 
+app.post("/", function(req,res){
+    return Nuevos.create({
+        nombre: req.body.nombre,
+        precio: req.body.precio
+    })
+    .then(Nuevos => res.status(200).send(Nuevos))
+    .catch(error => res.status().send(error))
+})
+
 let port;
-if (process.env.MODO === "PRODUCCION") {
+if (process.env.NODE_ENV === "production") {
     port = process.env.PORT_PROD;
 } else {
     port = process.env.PORT_DEV;
 }
 
 app.listen(port, function() {
-    console.log("Servidor Activo", port, process.env.MODO);
+    console.log("Servidor Activo", port, process.env.NODE_ENV);
 })
